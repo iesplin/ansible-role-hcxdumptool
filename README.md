1dd9e95521802813a56adad2e7799364786b1dfa
Ian Esplin <iesplin@users.noreply.github.com>
2019-01-09 10:22:52 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+ansible-role-hcxdumptool
+=========
+
+Ansible role to install hcxdumptool from the git repo - https://github.com/ZerBea/hcxdumptool
+
+Example Playbook
+----------------
+
+    - hosts: servers
+      roles:
+        - role: ansible-role-hcxdumptool
+
+License
+-------
+
+MIT
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hcxdumptool_file(host):
+    f = host.file('/usr/local/bin/hcxdumptool')
+    assert f.exists
